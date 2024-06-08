107b082c9fab7049f9dd0d5644973aab6559e072
AliRabeeMahmoud <eng.alirabee@gmail.com>
2024-06-08 19:34:34 +0200

    updated

--- src/test/java/com/example/integration/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/integration/repository/StudentRepositoryTest.java
@@ -11,42 +11,44 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
+//Testing the repository layer . not part of integration test
 class StudentRepositoryTest extends AbstractContainerBaseTest {
-
-    @Autowired
-    private StudentRepository studentRepository;
-
-    // JUnit for save student operation - BDD style
-    @Test
-    public void givenStudentObject_whenSave_thenReturnSavedStudent(){
-
-        // given - setup or precondition
-        Student student = Student.builder().firstName("Ramesh")
-                .lastName("fadatare").email("ramesh@gmail.com").build();
-
-        // when - action or the testing
-        Student savedStudent = studentRepository.save(student);
-
-        // then - very output
-        Assertions.assertNotNull(savedStudent);
-        Assertions.assertNotNull(savedStudent.getId());
-
-    }
-
-    // JUnit for save student operation - BDD style
-    @Test
-    public void givenStudentId_whenfindbyId_thenReturnSavedStudent(){
-
-        // given - setup or precondition
-        Student student = Student.builder().firstName("Ramesh")
-                .lastName("fadatare").email("ramesh@gmail.com").build();
-        Student savedStudent = studentRepository.save(student);
-
-        // when - action or the testing
-        Student studentDB = studentRepository.findById(student.getId()).get();
-
-        // then - very output
-        Assertions.assertNotNull(studentDB);
-    }
+//
+//    @Autowired
+//    private StudentRepository studentRepository;
+//
+//    // JUnit for save student operation - BDD style
+//    @Test
+//    public void givenStudentObject_whenSave_thenReturnSavedStudent(){
+//
+//        // given - setup or precondition
+//        Student student = Student.builder().firstName("Ramesh")
+//                .lastName("fadatare").email("ramesh@gmail.com").build();
+//
+//        // when - action or the testing
+//        Student savedStudent = studentRepository.save(student);
+//
+//        // then - very output
+//        Assertions.assertNotNull(savedStudent);
+//        Assertions.assertNotNull(savedStudent.getId());
+//
+//    }
+//
+//    // JUnit for save student operation - BDD style
+//    @Test
+//    public void givenStudentId_whenfindbyId_thenReturnSavedStudent(){
+//
+//        // given - setup or precondition
+//        Student student = Student.builder().firstName("Ramesh")
+//                .lastName("fadatare").email("ramesh@gmail.com").build();
+//        Student savedStudent = studentRepository.save(student);
+//
+//        // when - action or the testing
+//        Student studentDB = studentRepository.findById(student.getId()).get();
+//
+//        // then - very output
+//        Assertions.assertNotNull(studentDB);
+//    }
 
 }
